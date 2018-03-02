395cb783433a9c5293b5a3c6bd0e3428df74d017
whusym <whusym@gmail.com>
2018-03-02 10:48:38 -0500

    finished testing for preprocessing; added gitignore

--- johnson/preprocess.py
+++ johnson/preprocess.py
@@ -1,5 +1,12 @@
 '''
 This file includes preprocessing calls for the images.
+This preprocessing file provides the functions of loading and transforming
+image globs. Say, to load an image glob on Caesar server, just call:
+    images = load('01.01', 'caesar')
+To preprocess the images, we can just apply the functions on the loaded image glob:
+For instance,
+    images = grayScale(images)
+    images = medianFilter(images)
 '''
 
 import os
@@ -11,16 +18,20 @@ import scipy.ndimage as ndimg
 import skimage.filters as filters
 import skimage.morphology as morphology
 
-def load(setName):
+def load(setName, base):
     '''
     Reading all images from a file directory. argument neuroset indicates the name
     of the dataset. For instance, 00.00, 00.01, 02.00, etc.
     Return a matrix instantiating all images
     '''
-    # get the full name of the path; Here I coded to be the path on a AI server.
-    dirname = '/media/data2TB/jeremyshi/neurofinder.{}.test/images/'.format(setName)
+    # get the full name of the path;
+    if base == 'local':
+        dirname = "/Users/yuanmingshi/downloads/johnson/neurofinder.01.01/images/"
+    else:
+        dirname = '/media/data2TB/jeremyshi/neurofinder.{}.test/images/'.format(setName)
     # load images using opencv
     images = [cv2.imread(file) for file in glob('{}*.tiff'.format(dirname))]
+    print (len(images))
     return images
 
 def grayScale(images):
@@ -29,8 +40,8 @@ def grayScale(images):
     '''
     images = [cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) for image in images]  # convert to greyscale
     return images
-    
-def medianFilter(img):
+
+def medianFilter(images):
     '''
     Add a median filter into a image using _median filters_ and _morphology_ in skimage.
     Input: an image array.
@@ -38,8 +49,28 @@ def medianFilter(img):
     The method is adopted from:
     https://github.com/eds-uga/cbio4835-sp17/blob/master/lectures/Lecture23.ipynb
     '''
+    images = [_median(image) for image in images]
+    return images
+
+def _median(img):
+    '''
+    helper function for medianFilter method.
+    '''
     # i is the result of only keeping the values higher than the mean in the image
     i = img > np.mean(img)
     # Morphology has options on different shapes and values. In practice, square(3) or square(4) perform well.
     bin_median = filters.median(i, morphology.square(3))
     return bin_median
+
+if __name__ == '__main__':
+    images = load('_', 'local')
+    # print (images)
+    print(len(images), len(images[1]))
+    print(images[1].shape)
+    images = grayScale(images)
+    print(images[1].shape)
+    print(len(images), len(images[1]))
+    print(images[1])
+    images = medianFilter(images)
+    print(images[1].shape)
+    print(images[1])
