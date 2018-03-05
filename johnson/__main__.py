9397a26b1190dd57ea6190ee1fd07d8ec372b2f0
whusym <whusym@gmail.com>
2018-03-04 23:40:07 -0500

    added main pipeline with gitignore'

--- johnson/__main__.py
+++ johnson/__main__.py
@@ -1,2 +1,39 @@
 import argparse
 import johnson
+import tensorflow as tf
+
+def info(args):
+    '''
+    Print system info.
+    '''
+    print ('Python version:')
+    print (sys.version)
+    print ('Tensorflow version:')
+    print (tf.__version__)
+    print ('Tensorflow GPU Support')
+    print (tf.test.gpu_device_name())
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Neuron Segmentation',
+        argument_default=argparse.SUPPRESS
+    )
+    options = parser.add_subparsers()
+
+    # Print info
+    op = options.add_parser('info', description='print system info')
+    op.set_defaults(func = info)
+
+    # johnson nmf 
+
+    images = load('_', 'local')
+    # print (images)
+    print(len(images), len(images[1]))
+    print(images[1].shape)
+    images = grayScale(images)
+    print(images[1].shape)
+    print(len(images), len(images[1]))
+    print(images[1])
+    images = medianFilter(images)
+    print(images[1].shape)
+    print(images[1])
